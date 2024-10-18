86710b7e9c51ce83cac99ccaeafc87e12250b4cc
unknown <keel0004@e.ntu.edu.sg>
2024-10-18 12:10:51 +0800

    working on landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import LandingPage from "./pages/LandingPage"
 
-function App() {
+const App = () => {
+
+  useEffect(() => { 
+    console.log("Hello") 
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <LandingPage />
     </div>
   );
 }
