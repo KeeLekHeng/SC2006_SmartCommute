2d86a8b2f608eca386bc5ee4b542e996bae77d69
jangasd <amiroseth@gmail.com>
2024-11-05 12:09:18 +0800

    removed front and backend files

--- backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getReviews, addReview, updateUpvotes } = require('../controller/reviewController');
-const router = express.Router();
-
-router.get('/get', getReviews);
-router.post('/add', addReview);
-router.put('/upvote', updateUpvotes);
-
-module.exports = router;
